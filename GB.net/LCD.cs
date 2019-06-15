a0846aaee349094e927f6838d6bb9b5dd24e8c2e
giawa <giawa@hotmail.com>
2019-06-15 13:39:30 -0700

    New method that better deals with sprite ordering.  Still partially fails sprite tests.

--- GB.net/LCD.cs
+++ GB.net/LCD.cs
@@ -143,8 +143,13 @@ namespace GB
             bgTileData -= 0x8000;
             bgTileMap -= 0x8000;
 
+            // draw the background first
             int scx = _ram[0xff43];
             int scy = _ram[0xff42];
+
+            //Array.Clear(backgroundTexture, 160 * 4 * y, 160 * 4);
+            for (int i = 0; i < 160 * 4; i++)
+                backgroundTexture[i + 160 * 4 * y] = 255;
 
             // this is going to be slow!
             for (int x = 0; x < 160;)
@@ -158,6 +163,12 @@ namespace GB
                 int tiley = ty / 8;
 
                 int tile = _ram.VideoMemory[bgTileMap + tilex + tiley * 32];
+
+                if (bgTileData == 0x800)
+                {
+                    if (tile >= 0x80) tile -= 128;
+                    else tile += 128;
+                }
 
                 int j = ty % 8;
                 int k = 7 - (tx % 8);
@@ -184,7 +195,162 @@ namespace GB
                     x++;
                 }
             }
+
+            // now draw the sprites
+            int displayedSprites = 0;
+
+            Span<byte> oamMemory = new Span<byte>(_ram.SpecialPurpose, 0, 256);
+            byte palette0 = _ram[0xff48];
+            byte palette1 = _ram[0xff49];
+            for (int i = 0; i < 4; i++)
+            {
+                palette0lookup[i] = (palette0 >> (2 * i)) & 0x03;
+                palette1lookup[i] = (palette1 >> (2 * i)) & 0x03;
+            }
+
+            Array.Clear(activeSprites, 0, 10);
+            for (int i = 0; i < 160; i++) lineData[i] = -1;
+
+            for (int i = 0; i < 40; i++)
+            {
+                int spriteY = oamMemory[i * 4];
+                if (spriteY == 0 || spriteY >= 144 + 16) continue;
+                spriteY -= 16;
+
+                if (spriteY <= y && spriteY + 8 > y)
+                {
+                    // sprite exists on this line, populate the line data
+                    int spriteX = oamMemory[i * 4 + 1] - 8;
+                    var character = oamMemory[i * 4 + 2];
+                    var attr = oamMemory[i * 4 + 3];
+
+                    byte b1, b2;
+                    // check if y is flipped
+                    if ((attr & 0x40) == 0x40)
+                    {
+                        b1 = _ram.VideoMemory[character * 16 + (7 - (y - spriteY)) * 2];
+                        b2 = _ram.VideoMemory[character * 16 + (7 - (y - spriteY)) * 2 + 1];
+                    }
+                    else
+                    {
+                        b1 = _ram.VideoMemory[character * 16 + (y - spriteY) * 2];
+                        b2 = _ram.VideoMemory[character * 16 + (y - spriteY) * 2 + 1];
+                    }
+
+                    displayedSprites++;
+                    if (displayedSprites > 9) break;
+
+                    for (int x = Math.Max(0, spriteX); x < Math.Min(160, spriteX + 8); x++)
+                    {
+                        int _x = x - spriteX;
+                        int pixel;
+                        // check if x is flipped
+                        if ((attr & 0x20) == 0x20)
+                        {
+                            pixel = (((b2 >> _x) & 0x01) << 1) | ((b1 >> _x) & 0x01);
+                        }
+                        else
+                        {
+                            pixel = (((b2 >> (7 - _x)) & 0x01) << 1) | ((b1 >> (7 - _x)) & 0x01);
+                        }
+                        //if (pixel != 0)
+                        {
+                            pixel = (pixel == 0 ? 0 : ((attr & 0x10) == 0x10) ? palette1lookup[pixel] : palette0lookup[pixel]);
+                            //if (pixel != 0)
+                            {
+                                if (lineData[x] == -1) lineData[x] = pixel | (spriteX << 8);
+                                else if ((lineData[x] >> 8) > spriteX && pixel != 0) lineData[x] = pixel | (spriteX << 8);
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0, p = y * 160 * 4; i < 160; i++, p += 4)
+            {
+                if (lineData[i] == -1) continue;
+                int pixel = lineData[i] & 0xff;
+                if (pixel > 0)
+                {
+                    Array.Copy(activePalette[pixel], 0, backgroundTexture, p, 4);
+                }
+            }
+
+            /*for (int x = 0; x < 160; x++)
+            {
+                // only 10 sprites can be shown per line
+                if (displayedSprites > 9) break;
+
+                for (int i = 0; i < 40; i++)
+                {
+                    int spriteY = oamMemory[i * 4];//_ram[0xffe0 + i * 4];// + 16;
+                    // sprites with 0 or 160 y values do not affect total sprite count
+                    if (spriteY == 0 || spriteY >= 144 + 16) continue;
+                    spriteY -= 16;
+
+                    if (spriteY <= y && spriteY + 8 > y)
+                    {
+                        int spriteX = oamMemory[i * 4 + 1] - 8;//_ram[0xffe1 + i * 4] - 8;
+
+                        if (spriteX <= x && spriteX + 8 > x)
+                        {
+                            if (y == 48)
+                            {
+                                Console.WriteLine("stop");
+                            }
+                            var character = oamMemory[i * 4 + 2];
+                            var attr = oamMemory[i * 4 + 3];
+
+                            byte b1, b2;
+                            // check if y is flipped
+                            if ((attr & 0x40) == 0x40)
+                            {
+                                b1 = _ram.VideoMemory[character * 16 + (7 - (y - spriteY)) * 2];
+                                b2 = _ram.VideoMemory[character * 16 + (7 - (y - spriteY)) * 2 + 1];
+                            }
+                            else
+                            {
+                                b1 = _ram.VideoMemory[character * 16 + (y - spriteY) * 2];
+                                b2 = _ram.VideoMemory[character * 16 + (y - spriteY) * 2 + 1];
+                            }
+                            int p = x * 4 + y * 160 * 4;
+
+                            for (int _x = x - spriteX; _x < 8; _x++)
+                            {
+                                //int _x = x - spriteX;
+                                int pixel;
+                                // check if x is flipped
+                                if ((attr & 0x20) == 0x20)
+                                {
+                                    pixel = (((b2 >> _x) & 0x01) << 1) | ((b1 >> _x) & 0x01);
+                                }
+                                else
+                                {
+                                    pixel = (((b2 >> (7 - _x)) & 0x01) << 1) | ((b1 >> (7 -_x)) & 0x01);
+                                }
+                                if (pixel != 0)
+                                {
+                                    pixel = ((attr & 0x10) == 0x10) ? palette1lookup[pixel] : palette0lookup[pixel];
+                                    if (pixel != 0) Array.Copy(activePalette[pixel], 0, backgroundTexture, p, 4);
+                                }
+                                p += 4;
+                                x++;
+                            }
+                            x--;
+
+                            displayedSprites++;
+
+                            break;
+                        }
+                    }
+                }
+            }*/
         }
+
+        int[] palette0lookup = new int[4];
+        int[] palette1lookup = new int[4];
+        int[] activeSprites = new int[10];
+        int[] lineData = new int[160];
 
         private bool Tick4mhz()
         {
