c5075c4caae0f7f3aa56053adbc646772c311857
giawa <giawa@hotmail.com>
2019-06-15 13:41:28 -0700

    Fix up some bugs with the file dialog (re-create each time).
    Fix up osme bugs with ticks timing out prior to a frame completing.
    Add basic support for switching to 2x speed mode (GBC).
    Add stat interrupts.
    Disable drawing background to new window.

--- GB.net/Program.cs
+++ GB.net/Program.cs
@@ -68,10 +68,14 @@ namespace GB
 
             while (running)
             {
+                Stopwatch watch = Stopwatch.StartNew();
+
                 //try
                 {
                     while (SDL.SDL_PollEvent(out sdlEvent) != 0)
                     {
+                        //if (ram != null) ram.JoyPad = 0;
+
                         if (sdlEvent.type == SDL.SDL_EventType.SDL_QUIT)
                         {
                             running = false;
@@ -143,31 +147,30 @@ namespace GB
                             if (ImGui.MenuItem("Open File", "Ctrl+O"))
                             {
                                 showOpenDialog = true;
+                                openDialog = new FileDialog();
                                 ImGui.SetNextWindowPos(new Vector2(100, 100));
                             }
                             if (ImGui.MenuItem("Load Test ROM"))
                             {
                                 //Disassembler temp = new Disassembler(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\blargg_tests\cpu_instrs\cpu_instrs.gb", 0x200);
                                 Cartridge tetris = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\tetris.gb");
+                                Cartridge mario = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\mario.gb");
+                                Cartridge harvestMoon = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\harvestmoon.gb");
+                                Cartridge drmario = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\drmario.gb");
                                 Cartridge fullTest = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\blargg_tests\cpu_instrs\cpu_instrs.gb");
-                                Cartridge cpuTest1 = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\blargg_tests\cpu_instrs\individual\01-special.gb");
-                                Cartridge cpuTest2 = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\blargg_tests\cpu_instrs\individual\02-interrupts.gb");
-                                Cartridge cpuTest3 = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\blargg_tests\cpu_instrs\individual\03-op sp,hl.gb");
-                                Cartridge cpuTest4 = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\blargg_tests\cpu_instrs\individual\04-op r,imm.gb");
-                                Cartridge cpuTest5 = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\blargg_tests\cpu_instrs\individual\05-op rp.gb");
-                                Cartridge cpuTest6 = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\blargg_tests\cpu_instrs\individual\06-ld r,r.gb");
-                                Cartridge cpuTest7 = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\blargg_tests\cpu_instrs\individual\07-jr,jp,call,ret,rst.gb");
-                                Cartridge cpuTest8 = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\blargg_tests\cpu_instrs\individual\08-misc instrs.gb");
-                                Cartridge cpuTest9 = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\blargg_tests\cpu_instrs\individual\09-op r,r.gb");
-                                Cartridge cpuTest10 = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\blargg_tests\cpu_instrs\individual\10-bit ops.gb");
-                                Cartridge cpuTest11 = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\blargg_tests\cpu_instrs\individual\11-op a,(hl).gb");
+                                Cartridge instrTiming = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\blargg_tests\instr_timing\instr_timing.gb");
+                                Cartridge interruptTiming = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\blargg_tests\interrupt_time\interrupt_time.gb");
+                                Cartridge mooneye = new Cartridge(@"E:\Tutorials\GB.net\GB.net\bin\Debug\netcoreapp2.1\mooneye\manual-only\sprite_priority.gb");
                                 ram = new Memory();
                                 timer = new Timer(ram);
                                 cpu = new CPU(ram);
                                 lcd = new LCD(ram);
                                 cpu.LoadCartridge(fullTest);
                                 cpu.SetPC(0x100);
-                                
+                                //cpu.Breakpoints.Add(0x03EC);
+                                //cpu.Breakpoints.Add(0x2847);
+                                cpu.Breakpoints.Add(0xC056);
+
                                 // run a few clock cycles
                                 cpuState = cpu.CreateStateMachine().GetEnumerator();
                             }
@@ -196,7 +199,11 @@ namespace GB
                                 ram = new Memory();
                                 timer = new Timer(ram);
                                 cpu = new CPU(ram);
+                                lcd = new LCD(ram);
                                 cpu.LoadCartridge(gamecart);
+                                cpu.SetPC(0x100);
+
+                                cpuState = cpu.CreateStateMachine().GetEnumerator();
                             }
                         }
                     }
@@ -204,47 +211,68 @@ namespace GB
                     // run a full frame of gameboy data
                     if (cpuState != null)
                     {
-                        Stopwatch watch = Stopwatch.StartNew();
                         bool frameReady = false;
                         int ticks = 0;
 
-                        while (!frameReady && cpuState.MoveNext() && ticks < 10000)
+                        while (!frameReady && ticks < 17556 && cpuState.MoveNext())
                         {
+                            // support GBC double speed
+                            if ((ram[0xff4d] & 0x80) == 0x80)
+                            {
+                                // register all interrupts
+                                if (timer.TimerInterrupt)
+                                    ram[0xff0f] |= 0x04;
+                                timer.Tick1MHz();
+                                ram.Tick1MHz();
+                                cpuState.MoveNext();
+                            }
+
+                            // register all interrupts
+                            if (timer.TimerInterrupt)
+                                ram[0xff0f] |= 0x04;
+                            if (lcd.VBlankInterrupt)
+                            {
+                                ram[0xff0f] |= 0x01;
+                                lcd.VBlankInterrupt = false;
+                            }
+                            var ff41 = ram[0xff41];
+                            bool statInterrupt = false;
+                            if ((ff41 & 0x08) == 0x08 && lcd.CurrentMode == LCD.LCDMode.HBlank) statInterrupt = true;
+                            if ((ff41 & 0x10) == 0x10 && lcd.CurrentMode == LCD.LCDMode.VBlank) statInterrupt = true;
+                            if ((ff41 & 0x20) == 0x20 && lcd.CurrentMode == LCD.LCDMode.Mode2) statInterrupt = true;
+                            if ((ff41 & 0x44) == 0x44) statInterrupt = true;
+                            if (statInterrupt) ram[0xff0f] |= 0x02;
+                            var ff00 = (byte)(ram.GetJoyPad() & 0x0f);
+                            var xorff00 = ff00 ^ joypad;
+                            if (xorff00 != 0 && (joypad & xorff00) != 0) ram[0xff0f] |= 0x10;
+                            joypad = ff00;
+
                             frameReady = lcd.Tick1MHz();
                             ram.Tick1MHz();
                             timer.Tick1MHz();   // TODO:  "If a TMA write is executed with the same 
                                                 // timing of TMA being transferred to TIMA, then the TMA
                                                 // write goes to TIMA as well" (p 26 Gameboy Dev Manual)
 
-                            // register all interrupts
-                            cpu.Interrupts = (byte)(timer.TimerInterrupt ? 0x04 : 0x00);
-                            if (lcd.VBlankInterrupt)
-                            {
-                                cpu.Interrupts |= 0x01;
-                                lcd.VBlankInterrupt = false;
-                            }
-
                             ticks++;
                         }
 
-                        watch.Stop();
-
                         // did the program terminate?
-                        if (ticks >= 10000) ;
+                        if (ticks >= 17556 && !frameReady) ;
                         else if (!frameReady) cpuState = null;
                         else
                         {
                             //lcd.DumpTiles((ram[0xff40] & 0x10) == 0x10 ? 0x8000 : 0x8800);
+                            //Console.WriteLine("Running at {0}MHz", ticks / 1000000.0 * 60);
 
                             if (frameTexture != null) frameTexture.Dispose();
                             var bitmapHandle = GCHandle.Alloc(lcd.backgroundTexture, GCHandleType.Pinned);
                             frameTexture = new Texture(bitmapHandle.AddrOfPinnedObject(), 160, 144, PixelFormat.Rgba, PixelInternalFormat.Rgba);
                             bitmapHandle.Free();
 
-                            if (bgTexture != null) bgTexture.Dispose();
+                            /*if (bgTexture != null) bgTexture.Dispose();
                             bitmapHandle = GCHandle.Alloc(lcd.DumpBackground(), GCHandleType.Pinned);
                             bgTexture = new Texture(bitmapHandle.AddrOfPinnedObject(), 256, 256, PixelFormat.Rgba, PixelInternalFormat.Rgba);
-                            bitmapHandle.Free();
+                            bitmapHandle.Free();*/
                             //Console.WriteLine("Frame after {0}ms", watch.ElapsedMilliseconds);
                         }
                     }
@@ -279,6 +307,9 @@ namespace GB
                 {
                     Console.WriteLine(e.Message);
                 }*/
+
+                watch.Stop();
+                //Console.WriteLine("Frame took {0}ms", watch.ElapsedMilliseconds);
             }
 
             Gui.Dispose();
